7e9e159bb6c8f244687c657c86bb2e46482a110a
pontep <pontep.taveeshup@gmail.com>
2020-08-12 21:19:36 +0700

    empty test

--- cdk/main.test.ts
+++ cdk/main.test.ts
@@ -1,11 +1,8 @@
-import {MyChart} from './main';
-import {Testing} from "cdk8s";
+// import { MyChart } from "./main";
+// import { Testing } from "cdk8s";
 
-describe('Placeholder', () => {
-    test('Empty', () => {
-        const app = Testing.app();
-        const chart = new MyChart(app, 'test-chart');
-        const results = Testing.synth(chart)
-        expect(results).toMatchSnapshot();
+describe('LoadBalancedWebsite', () => {
+    test('empty', () => {
+
     });
 });

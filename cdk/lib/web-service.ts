b98d353f96c7ea2f6a76a85973305eae084ed43b
pontep <pontep.taveeshup@gmail.com>
2020-08-12 15:05:56 +0700

    add web-service.ts

---
+++ cdk/lib/web-service.ts
@@ -0,0 +1,70 @@
+import { Construct, Node } from 'constructs';
+import { Deployment, Service, IntOrString } from '../imports/k8s';
+
+export interface WebServiceOptions {
+  /**
+   * The Docker image to use for this service.
+   */
+  readonly image: string;
+
+  /**
+   * Number of replicas.
+   *
+   * @default 1
+   */
+  readonly replicas?: number;
+
+  /**
+   * External port.
+   *
+   * @default 80
+   */
+  readonly port?: number;
+
+  /**
+   * Internal port.
+   *
+   * @default 8080
+   */
+  readonly containerPort?: number;
+}
+
+export class WebService extends Construct {
+  constructor(scope: Construct, ns: string, options: WebServiceOptions) {
+    super(scope, ns);
+
+    const port = options.port || 80;
+    const containerPort = options.containerPort || 8080;
+    const label = { app: Node.of(this).uniqueId };
+    const replicas = options.replicas ?? 1;
+
+    new Service(this, 'service', {
+      spec: {
+        type: 'LoadBalancer',
+        ports: [ { port, targetPort: IntOrString.fromNumber(containerPort) } ],
+        selector: label
+      }
+    });
+
+    new Deployment(this, 'deployment', {
+      spec: {
+        replicas,
+        selector: {
+          matchLabels: label
+        },
+        template: {
+          metadata: { labels: label },
+          spec: {
+            containers: [
+              {
+                name: 'app',
+                image: options.image,
+                ports: [ { containerPort } ]
+              }
+            ]
+          }
+        }
+      }
+    });
+  }
+}
--- cdk/main.ts
+++ cdk/main.ts
@@ -1,46 +1,12 @@
+import { Chart } from 'cdk8s';
 import { Construct } from 'constructs';
-import { App, Chart } from 'cdk8s';
+import { WebService } from './lib/web-service';
 
-// imported constructs
-import { Deployment, Service, IntOrString } from './imports/k8s';
+export class MyChart extends Chart {
+  constructor(scope: Construct, ns: string) {
+    super(scope, ns);
 
-class MyChart extends Chart {
-  constructor(scope: Construct, name: string) {
-    super(scope, name);
-
-    const label = { app: 'hello-k8s' };
-
-    new Service(this, 'service', {
-      spec: {
-        type: 'LoadBalancer',
-        ports: [ { port: 80, targetPort: IntOrString.fromNumber(8080) } ],
-        selector: label
-      }
-    });
-
-    new Deployment(this, 'deployment', {
-      spec: {
-        replicas: 2,
-        selector: {
-          matchLabels: label
-        },
-        template: {
-          metadata: { labels: label },
-          spec: {
-            containers: [
-              {
-                name: 'hello-kubernetes',
-                image: 'paulbouwer/hello-kubernetes:1.7',
-                ports: [ { containerPort: 8080 } ]
-              }
-            ]
-          }
-        }
-      }
-    });
+    new WebService(this, 'hello', { image: 'paulbouwer/hello-kubernetes:1.7', replicas: 2 });
+    new WebService(this, 'ghost', { image: 'ghost', containerPort: 2368 });
   }
 }
-
-const app = new App();
-new MyChart(app, 'hello');
-app.synth();

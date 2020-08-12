74e7acaa12c1994bdedb668eeaf44e385fbbdd76
pontep <pontep.taveeshup@gmail.com>
2020-08-12 14:16:27 +0700

    add hello k8s api

--- cdk/main.ts
+++ cdk/main.ts
@@ -1,15 +1,46 @@
 import { Construct } from 'constructs';
 import { App, Chart } from 'cdk8s';
 
-export class MyChart extends Chart {
+// imported constructs
+import { Deployment, Service, IntOrString } from './imports/k8s';
+
+class MyChart extends Chart {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    // define resources here
+    const label = { app: 'hello-k8s' };
 
+    new Service(this, 'service', {
+      spec: {
+        type: 'LoadBalancer',
+        ports: [ { port: 80, targetPort: IntOrString.fromNumber(8080) } ],
+        selector: label
+      }
+    });
+
+    new Deployment(this, 'deployment', {
+      spec: {
+        replicas: 2,
+        selector: {
+          matchLabels: label
+        },
+        template: {
+          metadata: { labels: label },
+          spec: {
+            containers: [
+              {
+                name: 'hello-kubernetes',
+                image: 'paulbouwer/hello-kubernetes:1.7',
+                ports: [ { containerPort: 8080 } ]
+              }
+            ]
+          }
+        }
+      }
+    });
   }
 }
 
 const app = new App();
-new MyChart(app, 'cdk');
+new MyChart(app, 'hello');
 app.synth();

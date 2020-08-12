4b1fcdce3a871af5321661c426b51a3e6efad327
pontep <pontep.taveeshup@gmail.com>
2020-08-12 15:10:45 +0700

    cdk OK

--- cdk/main.ts
+++ cdk/main.ts
@@ -1,12 +1,46 @@
-import { Chart } from 'cdk8s';
 import { Construct } from 'constructs';
-import { WebService } from './lib/web-service';
+import { App, Chart } from 'cdk8s';
+
+// imported constructs
+import { Deployment, Service, IntOrString } from './imports/k8s';
 
 export class MyChart extends Chart {
-  constructor(scope: Construct, ns: string) {
-    super(scope, ns);
+  constructor(scope: Construct, name: string) {
+    super(scope, name);
 
-    new WebService(this, 'hello', { image: 'paulbouwer/hello-kubernetes:1.7', replicas: 2 });
-    new WebService(this, 'ghost', { image: 'ghost', containerPort: 2368 });
+    const label = { app: 'hello-k8s' };
+
+    new Service(this, 'service', {
+      spec: {
+        type: 'LoadBalancer',
+        ports: [ { port: 80, targetPort: IntOrString.fromNumber(8080) } ],
+        selector: label
+      }
+    });
+
+    new Deployment(this, 'deployment', {
+      spec: {
+        replicas: 2,
+        selector: {
+          matchLabels: label
+        },
+        template: {
+          metadata: { labels: label },
+          spec: {
+            containers: [
+              {
+                name: 'hello-kubernetes',
+                image: 'paulbouwer/hello-kubernetes:1.7',
+                ports: [ { containerPort: 8080 } ]
+              }
+            ]
+          }
+        }
+      }
+    });
   }
 }
+
+const app = new App();
+new MyChart(app, 'hello');
+app.synth();

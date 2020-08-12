c6c9bfb824e502653d746bb9fbd650f4e14975b2
pontep <pontep.taveeshup@gmail.com>
2020-08-12 16:16:41 +0700

    add dinnynote cdk8s

--- cdk/main.ts
+++ cdk/main.ts
@@ -1,16 +1,50 @@
 import { Construct } from 'constructs';
 import { App, Chart } from 'cdk8s';
 import { WebService } from './lib/web-service';
+// import { Deployment, Service, IntOrString } from './imports/k8s';
 
 export class MyChart extends Chart {
   constructor(scope: Construct, ns: string) {
     super(scope, ns);
 
-    new WebService(this, 'hello', { image: 'paulbouwer/hello-kubernetes:1.7', replicas: 2 });
-    new WebService(this, 'ghost', { image: 'ghost', containerPort: 2368 });
+    // DinnyNote Front-end
+    new WebService(this, 'frontend', { image: 'pontep/client', replicas: 2 });
+
+    // DinnyNote Back-end
+    new WebService(this, 'backend', { image: 'pontep/backend', port: 9000, containerPort: 9000});
+    // const label = { app: 'backend' };
+
+    // new Service(this, 'service', {
+    //   spec: {
+    //     type: 'LoadBalancer',
+    //     ports: [ { port: 9000, targetPort: IntOrString.fromNumber(9000) } ],
+    //     selector: label
+    //   }
+    // });
+
+    // new Deployment(this, 'deployment', {
+    //   spec: {
+    //     replicas: 1,
+    //     selector: {
+    //       matchLabels: label
+    //     },
+    //     template: {
+    //       metadata: { labels: label },
+    //       spec: {
+    //         containers: [
+    //           {
+    //             name: 'app',
+    //             image: 'pontep/backend',
+    //             ports: [ { containerPort: 9000 } ]
+    //           }
+    //         ]
+    //       }
+    //     }
+    //   }
+    // });
   }
 }
 
 const app = new App();
-new MyChart(app, 'hello');
+new MyChart(app, 'dinnynote');
 app.synth();

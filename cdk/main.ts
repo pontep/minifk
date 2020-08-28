ad57f4f94940a4099be21550967a2c73565225f5
pontep <pontep.taveeshup@gmail.com>
2020-08-28 16:25:15 +0700

    make a pod

--- cdk/main.ts
+++ cdk/main.ts
@@ -1,21 +1,49 @@
-import { Construct } from 'constructs';
-import { App, Chart } from 'cdk8s';
-import { WebService } from './lib/web-service';
-import { MicroService } from './lib/micro-service';
+import { Construct } from "constructs";
+import { App, Chart } from "cdk8s";
+// import { WebService } from "./lib/web-service";
+// import { MicroService } from "./lib/micro-service";
+import { Pod } from "./imports/k8s";
 // import { Deployment, Service, IntOrString } from './imports/k8s';
 
 export class MyChart extends Chart {
   constructor(scope: Construct, ns: string) {
     super(scope, ns);
 
-    // DinnyNote Front-end
-    new WebService(this, 'front', { image: 'pontep/client', replicas: 2, loadBalancerIP: '172.17.0.208' });
+    // Define
+    const namespace = "b6000783";
+    // Pod
+    const pod_image = "paulbouwer/hello-kubernetes:1.8";
+    const pod_label = "hello";
 
-    // DinnyNote Back-end
-    new MicroService(this, 'back', { image: 'pontep/backend', port: 9000, containerPort: 9000, loadBalancerIP: '172.17.0.209'});
+    new Pod(this, pod_label, {
+      metadata: {
+        labels: {
+          app: pod_label,
+          creator: "paulbouwer",
+          environment: "test",
+          week: "4",
+          reference: "chanwit",
+        },
+        namespace: namespace,
+        name: pod_label,
+      },
+      spec: {
+        containers: [
+          {
+            name: pod_label,
+            image: pod_image,
+            ports: [
+              {
+                containerPort: 80,
+              },
+            ],
+          },
+        ],
+      },
+    });
   }
 }
 
 const app = new App();
-new MyChart(app, 'dinnynote');
+new MyChart(app, "dinnynote");
 app.synth();

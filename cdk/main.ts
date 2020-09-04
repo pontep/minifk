46b108c3ea89edbe645381a02ef6e7e61730fe27
pontep <pontep.taveeshup@gmail.com>
2020-09-04 19:34:49 +0700

    pod demo1

--- cdk/main.ts
+++ cdk/main.ts
@@ -29,7 +29,7 @@ export class MyChart extends Chart {
 
     // Pod
     const pod_image = "paulbouwer/hello-kubernetes:1.8";
-    const pod_name = "hellodin"
+    const pod_name = "demo1"
 
     new Pod(this, pod_name, {
       metadata: {

a871557045b926073e960537c3c2b71e3b2df12a
pontep <pontep.taveeshup@gmail.com>
2020-09-11 18:54:41 +0700

    cdk8s-plus OK

--- cdk-plus/main.ts
+++ cdk-plus/main.ts
@@ -1,6 +1,6 @@
 import * as kplus from 'cdk8s-plus';
 import * as cdk8s from 'cdk8s';
-// import * as path from 'path';
+import * as path from 'path';
 
 // our cdk app
 const app = new cdk8s.App();
@@ -8,15 +8,15 @@ const app = new cdk8s.App();
 // our kuberentes chart
 const chart = new cdk8s.Chart(app, 'DinChart');
 
-// // lets create a volume that contains our app.
-// // we use a trick with a config map!
-// const appData = new kplus.ConfigMap(chart, 'AppData');
-// appData.addDirectory(path.join(__dirname, 'app'));
+// lets create a volume that contains our app.
+// we use a trick with a config map!
+const appData = new kplus.ConfigMap(chart, 'AppData');
+appData.addDirectory(path.join(__dirname, 'app'));
 
-// const appVolume = kplus.Volume.fromConfigMap(appData);
+const appVolume = kplus.Volume.fromConfigMap(appData);
 
-// // now we create a container that runs our app
-// const appPath = '/var/lib/app';
+// now we create a container that runs our app
+const appPath = '/var/lib/app';
 const port = 80;
 const container = new kplus.Container({
   image: 'node:14.4.0-alpine3.12',
@@ -24,8 +24,8 @@ const container = new kplus.Container({
   port: port,
 })
 
-// // make the app accessible to the container
-// container.mount(appPath, appVolume);
+// make the app accessible to the container
+container.mount(appPath, appVolume);
 
 // now lets create a deployment to run a few instances of this container
 const deployment = new kplus.Deployment(chart, 'Deployment', {

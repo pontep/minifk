11bcfb74c0a606c66dace92cacc3845d7420eeb9
pontep <pontep.taveeshup@gmail.com>
2020-10-08 09:20:18 +0700

    add istio, jenkins crds

--- cdk-plus/main.ts
+++ cdk-plus/main.ts
@@ -1,33 +1,58 @@
-import * as kplus from 'cdk8s-plus';
-import * as cdk8s from 'cdk8s';
+import { Construct } from 'constructs';
+import { App, Chart } from 'cdk8s';
 
-// our cdk app
-const app = new cdk8s.App();
+import { IstioOperator } from './imports/install.istio.io/istiooperator'
+import { Policy } from './imports/authentication.istio.io/policy'
+// import { Rule } from './imports/config.istio.io/rule'
+import { Gateway, GatewaySpecServersTlsMode } from './imports/networking.istio.io/gateway'
+// import { ServiceRole } from './imports/rbac.istio.io/servicerole'
+// import { AuthorizationPolicy } from './imports/security.istio.io/authorizationpolicy'
+export class HelloKube extends Chart {
+    constructor(scope: Construct, id: string) {
+        super(scope, id);
 
-// our kuberentes chart
-const chart = new cdk8s.Chart(app, 'cdk8s-plus');
-
-const deployment = new kplus.Deployment(chart, 'deployment', {
-  spec: {
-    replicas: 3,
-    podSpecTemplate: {
-      containers: [
-        new kplus.Container({
-          name: 'hello-kubernetes',
-          image: 'paulbouwer/hello-kubernetes:1.8',
-          port: 8080,
+        new IstioOperator(this, 'istiooperator', {
+            spec: {
+                profile: 'demo'
+            }
         })
-      ]
+        // Gateway example: https://istio.io/v1.5/docs/concepts/traffic-management/#gateways
+        new Gateway(this, 'istio-gateway', {
+            spec: {
+                selector: {
+                    app: 'my-gateway-controller'
+                },
+                servers: [
+                    {
+                        port: {
+                            number: 443,
+                            name: 'https',
+                            protocol: 'HTTPS'
+                        },
+                        hosts: [
+                            'ext-host.example.com'
+                        ],
+                        tls: {
+                            mode: GatewaySpecServersTlsMode.SIMPLE,
+                            serverCertificate: '/tmp/tls.crt',
+                            privateKey: '/tmp/tls.key'
+                        }
+                    }
+                ]
+            }
+        })
+
+        new Policy(this, 'istio-policy', {
+            spec: {
+
+            }
+        })
+
+
+
     }
-  }
-});
+}
 
-deployment.expose({
-  port: 80,
-  serviceType: kplus.ServiceType.LOAD_BALANCER
-})
-
-export { app }
-
-// we are done, synth
+const app = new App();
+new HelloKube(app, 'hello-k8s');
 app.synth();
